e8af455d4d52329902ebd42381622a0fb24b4ec7
Rui Mangas Pereira <ruijosemangas@gmail.com>
2017-02-11 13:41:16 +0000

    added more bitmap class specs

--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe Bitmap do
-  before { @bitmap = Bitmap.new(5,5) }
+  before { 
+    @bitmap = Bitmap.new(5,5)
+  }
 
   subject { @bitmap }
 
@@ -36,11 +38,12 @@ describe Bitmap do
       subject.paint_pixel(2, 3, 'R')
     end
 
-    it 'should paint the (2,3) pixel' do
+    it 'should paint the (2,3) pixel with red' do
+      c = 'R'
       expected_bitmap = [
         [0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0],
-        [0, 'R', 0, 0, 0],
+        [0, c, 0, 0, 0],
         [0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0]
       ]
@@ -48,15 +51,70 @@ describe Bitmap do
       expect(subject.print).to eq(expected_bitmap)
     end
 
-    it 'should paint the (5,5) pixel' do
-      subject.paint_pixel(5, 5, 'R')
+    it 'should paint the (5,5) pixel with red' do
+      c = 'R'
+      subject.paint_pixel(5, 5, c)
 
       expected_bitmap = [
         [0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0],
-        [0, 'R', 0, 0, 0],
+        [0, c, 0, 0, 0],
         [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 'R']
+        [0, 0, 0, 0, c]
+      ]
+
+      expect(subject.print).to eq(expected_bitmap)
+    end
+  end
+
+  describe 'paint a horizontal line' do
+    before do
+      subject.paint_row(2, 1, 5, 'R')
+    end
+
+    it 'should paint the second row with red' do
+      c = 'R'
+      expected_bitmap = [
+        [0, 0, 0, 0, 0],
+        [c, c, c, c, c],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+      ]
+
+      expect(subject.print).to eq(expected_bitmap)
+    end
+  end
+
+  describe 'paint a vertical line' do
+    before do
+      subject.paint_column(2, 1, 5, 'Y')
+    end
+
+    it 'should paint the second column with yellow' do
+      c = 'Y'
+      expected_bitmap = [
+        [0, c, 0, 0, 0],
+        [0, c, 0, 0, 0],
+        [0, c, 0, 0, 0],
+        [0, c, 0, 0, 0],
+        [0, c, 0, 0, 0]
+      ]
+
+      expect(subject.print).to eq(expected_bitmap)
+    end
+  end
+
+  describe 'bitmap clear' do
+    it 'should clear the bitmap' do
+      subject.clear_image
+
+      expected_bitmap = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
       ]
 
       expect(subject.print).to eq(expected_bitmap)
